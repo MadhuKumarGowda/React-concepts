79c973c6b2b417cca2a87c25171bee021bef31e4
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2025-09-18 22:59:09 -0600

    Added State Hook to retain the selected index of list

--- react-app/src/components/ListGroups.tsx
+++ react-app/src/components/ListGroups.tsx
@@ -7,22 +7,32 @@
  * This component demonstrates mapping over an array to render a list in React.
  */
 
-import type { MouseEvent } from "react";
+import { useState, type MouseEvent } from "react";
 function ListGroups() {
   // Array of city names to display in the list
   const items = ["New York", "London", "Tokyo", "Paris", "Sydney"];
 
   const empty_message = "No items to display";
 
+  const [listIndex, setlistIndex] = useState(-1);
+
   /**
    * Handles the click event for a list group item.
    *
    * @param event - The mouse event triggered by clicking the item.
    */
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // const handleClick = (event: MouseEvent) => {
+  //   console.log(event);
+  // };
   // Map each city name to a <li> element with a unique key
-  const list_items = items.map((item) => (
-    <li className="list-group-item" key={item} onClick={handleClick}>
+  const list_items = items.map((item, index) => (
+    <li
+      className={
+        listIndex === index ? "list-group-item active" : "list-group-item"
+      }
+      key={item}
+      onClick={() => setlistIndex(index)}
+    >
       {item}
     </li>
   ));
