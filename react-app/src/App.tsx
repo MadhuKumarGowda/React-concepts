137ef1b0303105ea05e97c501f6994b94ab05c38
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2025-10-26 15:59:35 -0600

    Controller Component using State Hook

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,25 +1,12 @@
 // App.tsx
 // Main entry point of the React application.
 
-import { useState } from "react";
-import StateHook from "./components/StateHook";
-import Navbar from "./components/Navbar";
-import Product from "./components/Product";
 import Form from "./components/Form";
 function App() {
-  const [product, setProduct] = useState(["Product1", "Product2", "Product3"]);
-  const handleReset = () => {
-    setProduct([]);
-  };
   return (
     <>
-      <h2>Understanding The State Hook</h2>
-      <StateHook />
-      <br />
-      <hr />
-      <h3> Passing state between components</h3>
-      <Navbar productsCount={product.length} />
-      <Product productItems={product} onClear={handleReset} />
+      <h2>Understanding The Controller Component</h2>
+
       <Form />
     </>
   );
